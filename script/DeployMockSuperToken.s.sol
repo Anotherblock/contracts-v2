56b3749a6cac0b7b3ae35b4f5e9c0bed896cf8ae
Pilou <76021631+0xPilou@users.noreply.github.com>
2023-04-03 17:05:22 +0200

    deploy ABSuperToken

--- script/DeployMockSuperToken.s.sol
+++ script/DeployMockSuperToken.s.sol
@@ -1,22 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.18;
 
 import "forge-std/Script.sol";
-import "../src/Staking.sol";
-import "../test/mocks/MockERC20.sol";
+import "../test/mocks/ABSuperToken.sol";
 
-contract DeployPoc is Script {
+contract DeployMockSuperToken is Script {
+    error INCORRECT_NETWORK();
+
+    address SF_HOST_OPTIMISM_GOERLI = 0xE40983C2476032A0915600b9472B3141aA5B5Ba9;
+
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        Staking staking = new Staking();
-        MockERC20 merc20 = new MockERC20();
-
-        uint256 amount = 10e18;
-
-        merc20.approve(address(staking), amount);
-        staking.stake(address(merc20), amount);
+        ABSuperToken abSuperToken = new ABSuperToken(SF_HOST_OPTIMISM_GOERLI);
 
         vm.stopBroadcast();
     }
