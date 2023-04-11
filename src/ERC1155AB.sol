031a7cdb6eac7b5aca6aadea9035fced4d04be95
Pilou <76021631+0xPilou@users.noreply.github.com>
2023-04-11 14:59:57 +0200

    added phase support in ERC1155AB (untested)

--- src/ERC1155AB.sol
+++ src/ERC1155AB.sol
@@ -8,15 +8,46 @@ import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/Own
 /* Custom Interfaces */
 import {IABRoyalty} from "./interfaces/IABRoyalty.sol";
 
-struct TokenDetails {
-    uint256 mintedSupply;
-    uint256 maxSupply;
-    uint256 price;
-    string uri;
-}
-// uint256 dropId;
-
 contract ERC1155AB is ERC1155Upgradeable, OwnableUpgradeable {
+    struct TokenDetails {
+        uint256 mintedSupply;
+        uint256 maxSupply;
+        uint256 numOfPhase;
+        mapping(uint256 phaseId => Phase phase) phases;
+        string uri;
+    }
+    // uint256 dropId; (?)
+
+    /**
+     * @notice
+     *  Phase Structure format
+     *
+     * @param phaseStart : timestamp at which the phase starts
+     * @param phaseStart : timestamp at which the phase ends
+     * @param price : price for one token during the phase
+     * @param maxMint : maximum number of token to be minted per user during the phase
+     * @param merkle : merkle tree root containing user address and associated parameters
+     */
+    struct Phase {
+        uint256 phaseStart;
+        uint256 phaseEnd;
+        uint256 price;
+        uint256 maxMint;
+        bytes32 merkle;
+    }
+
+    error DropSoldOut();
+    error NotEnoughTokensAvailable();
+    error IncorrectETHSent();
+    error NoSaleInProgress();
+    error MaxMintPerAddress();
+    error NotEligible();
+    error InvalidParameter();
+    error PhasesNotSet();
+    error SaleNotStarted();
+
+    event UpdatedPhase(uint256 numOfPhase);
+
     //     _____ __        __
     //    / ___// /_____ _/ /____  _____
     //    \__ \/ __/ __ `/ __/ _ \/ ___/
@@ -30,11 +61,6 @@ contract ERC1155AB is ERC1155Upgradeable, OwnableUpgradeable {
     mapping(uint256 tokenId => TokenDetails tokenDetails) public tokensDetails;
 
     uint8 public constant IMPLEMENTATION_VERSION = 1;
-
-    error DropSoldOut();
-    error NotEnoughTokensAvailable();
-    error IncorrectETHSent();
-    error IncorrectParameter();
 
     //     ______                 __                  __
     //    / ____/___  ____  _____/ /________  _______/ /_____  _____
@@ -68,6 +94,10 @@ contract ERC1155AB is ERC1155Upgradeable, OwnableUpgradeable {
     function mint(address _to, uint256 _tokenId, uint256 _quantity) external payable {
         TokenDetails storage tokenDetails = tokensDetails[_tokenId];
 
+        uint256 phaseId = _getActivePhaseId(_tokenId);
+
+        Phase memory phase = tokenDetails.phases[phaseId];
+
         // Check if the drop is not sold-out
         if (tokenDetails.mintedSupply == tokenDetails.maxSupply) {
             revert DropSoldOut();
@@ -79,7 +109,7 @@ contract ERC1155AB is ERC1155Upgradeable, OwnableUpgradeable {
         }
 
         // Check that user is sending the correct amount of ETH (will revert if user send too much or not enough)
-        if (msg.value != tokenDetails.price * _quantity) {
+        if (msg.value != phase.price * _quantity) {
             revert IncorrectETHSent();
         }
 
@@ -99,11 +129,12 @@ contract ERC1155AB is ERC1155Upgradeable, OwnableUpgradeable {
     //  \____/_/ /_/_/\__, /   \____/ |__/|__/_/ /_/\___/_/
     //               /____/
 
-    function initDrop(uint256 _price, uint256 _maxSupply, uint256 _mintGenesis, string memory _uri)
-        external
-        onlyOwner
-    {
-        tokensDetails[tokenCount] = TokenDetails(0, _maxSupply, _price, _uri);
+    function initDrop(uint256 _maxSupply, uint256 _mintGenesis, string memory _uri) external onlyOwner {
+        TokenDetails storage newTokenDetails = tokensDetails[tokenCount];
+
+        newTokenDetails.maxSupply = _maxSupply;
+        newTokenDetails.uri = _uri;
+        // tokensDetails[tokenCount] = TokenDetails({mintedSupply: 0, maxSupply: _maxSupply, numOfPhase: 0, uri: _uri});
 
         if (_hasPayout()) {
             // Initialize payout index
@@ -112,13 +143,43 @@ contract ERC1155AB is ERC1155Upgradeable, OwnableUpgradeable {
 
         // Mint Genesis tokens to the drop owner
         if (_mintGenesis > 0) {
-            if (_mintGenesis > _maxSupply) revert IncorrectParameter();
+            if (_mintGenesis > _maxSupply) revert InvalidParameter();
             tokensDetails[tokenCount].mintedSupply += _mintGenesis;
             _mint(msg.sender, tokenCount, _mintGenesis, "");
         }
 
         // Increment tokenDetails count
         tokenCount++;
+    }
+
+    /**
+     * @notice
+     *  Set the sale phases for drop
+     *
+     * @param _tokenId : token ID for which the phases are set
+     * @param _phases : array of phases to be set
+     */
+    function setDropPhases(uint256 _tokenId, Phase[] memory _phases) external onlyOwner {
+        TokenDetails storage tokenDetails = tokensDetails[_tokenId];
+
+        tokenDetails.numOfPhase = _phases.length;
+
+        uint256 previousPhaseStart = 0;
+
+        uint256 length = _phases.length;
+
+        for (uint256 i = 0; i < length; ++i) {
+            Phase memory phase = _phases[i];
+
+            // Check parameter correctness (phase order and consistence between phase start & phase end)
+            if (phase.phaseStart > phase.phaseEnd || phase.phaseStart <= previousPhaseStart) {
+                revert InvalidParameter();
+            }
+            tokenDetails.phases[i] = phase;
+            previousPhaseStart = phase.phaseStart;
+        }
+
+        emit UpdatedPhase(length);
     }
 
     function setTokenUri(uint256 _tokenId, string memory _uri) external onlyOwner {
@@ -140,6 +201,30 @@ contract ERC1155AB is ERC1155Upgradeable, OwnableUpgradeable {
     //    / // __ \/ __/ _ \/ ___/ __ \/ __ `/ /  / /_  / / / / __ \/ ___/ __/ / __ \/ __ \/ ___/
     //  _/ // / / / /_/  __/ /  / / / / /_/ / /  / __/ / /_/ / / / / /__/ /_/ / /_/ / / / (__  )
     // /___/_/ /_/\__/\___/_/  /_/ /_/\__,_/_/  /_/    \__,_/_/ /_/\___/\__/_/\____/_/ /_/____/
+
+    /**
+     * @notice
+     *  Returns currently active phase ID
+     *
+     * @param _tokenId : token ID for which the phases are corresponding to
+     *
+     * @return : active phase ID
+     */
+    function _getActivePhaseId(uint256 _tokenId) internal view returns (uint256) {
+        TokenDetails storage tokenDetails = tokensDetails[_tokenId];
+
+        if (tokenDetails.numOfPhase == 0) revert PhasesNotSet();
+
+        if (tokenDetails.phases[0].phaseStart > block.timestamp) revert SaleNotStarted();
+
+        for (uint256 i = 0; i < tokenDetails.numOfPhase; ++i) {
+            if (
+                tokenDetails.phases[i].phaseStart <= block.timestamp
+                    && tokenDetails.phases[i].phaseEnd > block.timestamp
+            ) return i;
+        }
+        revert NoSaleInProgress();
+    }
 
     function _hasPayout() internal view returns (bool) {
         return address(royaltyContract) != address(0);
