ddc4b6fd62147f1a3cde9cabed0f494dde9f4970
Pilou <76021631+0xPilou@users.noreply.github.com>
2023-04-18 17:59:27 +0200

    updated deployment scripts

--- script/02-deploy-AnotherCloneFactory.s.sol
+++ script/02-deploy-AnotherCloneFactory.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.18;
 
 import "forge-std/Script.sol";
 import "../src/ABRoyalty.sol";
+import "../src/ABVerifier.sol";
 import "../src/AnotherCloneFactory.sol";
 import "../src/ERC721AB.sol";
 import "../src/ERC1155AB.sol";
@@ -11,15 +12,23 @@ contract DeployAnotherCloneFactory is Script {
     function run() external {
         // Account to deploy from
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address allowlistSigner = vm.addr(deployerPrivateKey);
+
         vm.startBroadcast(deployerPrivateKey);
 
         // Deploy Implementation Contracts
         ERC721AB erc721Impl = new ERC721AB();
         ERC1155AB erc1155Impl = new ERC1155AB();
         ABRoyalty royaltyImpl = new ABRoyalty();
+        ABVerifier abVerifier = new ABVerifier(allowlistSigner);
 
         // Deploy AnotherCloneFactory
-        new AnotherCloneFactory(address(erc721Impl), address(erc1155Impl), address(royaltyImpl));
+        new AnotherCloneFactory(
+            address(abVerifier), 
+            address(erc721Impl), 
+            address(erc1155Impl), 
+            address(royaltyImpl)
+        );
 
         vm.stopBroadcast();
     }
