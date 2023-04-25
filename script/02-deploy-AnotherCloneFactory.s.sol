f80980b7e64c270e35aa84a6972b39254c8a1f05
Pilou <76021631+0xPilou@users.noreply.github.com>
2023-04-25 17:20:07 +0200

    update deploy script (drop registry)

--- script/02-deploy-AnotherCloneFactory.s.sol
+++ script/02-deploy-AnotherCloneFactory.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.18;
 import "forge-std/Script.sol";
 import "../src/ABRoyalty.sol";
 import "../src/ABVerifier.sol";
+import "../src/ABDropRegistry.sol";
 import "../src/AnotherCloneFactory.sol";
 import "../src/ERC721AB.sol";
 import "../src/ERC1155AB.sol";
@@ -24,15 +25,19 @@ contract DeployAnotherCloneFactory is Script {
         ERC1155AB erc1155Impl = new ERC1155AB();
         ABRoyalty royaltyImpl = new ABRoyalty();
         ABVerifier abVerifier = new ABVerifier(allowlistSigner);
+        ABDropRegistry abDropRegistry = new ABDropRegistry(OPTIMISM_GOERLI_CHAIN_ID * DROP_ID_OFFSET);
 
         // Deploy AnotherCloneFactory
-        new AnotherCloneFactory(
-            OPTIMISM_GOERLI_CHAIN_ID * DROP_ID_OFFSET,
+        AnotherCloneFactory anotherCloneFactory = new AnotherCloneFactory(
+            address(abDropRegistry), 
             address(abVerifier), 
             address(erc721Impl), 
             address(erc1155Impl), 
             address(royaltyImpl)
         );
+
+        // Set AnotherCloneFactory address in ABDropRegistry contract
+        abDropRegistry.setAnotherCloneFactory(address(anotherCloneFactory));
 
         vm.stopBroadcast();
     }
