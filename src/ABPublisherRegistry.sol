8312991c7febea5764cd62d6c95e42c7390cb21e
Pilou <76021631+0xPilou@users.noreply.github.com>
2023-04-26 12:39:24 +0200

    draft publisher registry

---
+++ src/ABPublisherRegistry.sol
@@ -0,0 +1,167 @@
+//                            ██████████████████████████████████
+//                            ██████████████████████████████████
+//                            ██████████████████████████████████
+//                            ██████████████████████████████████
+//                            ██████████████████████████████████
+//                            ██████████████████████████████████
+//                            ██████████████████████████████████
+//                            ██████████████████████████████████
+//                            ██████████████████████████████████
+//                            ████████████████████████          ██████████
+//                            ████████████████████████          ██████████
+//                            ████████████████████████          ██████████
+//                            ████████████████████████          ██████████
+//                                                    ████████████████████
+//                                                    ████████████████████
+//                                                    ████████████████████
+//                                                    ████████████████████
+//
+//
+//  █████╗ ███╗   ██╗ ██████╗ ████████╗██╗  ██╗███████╗██████╗ ██████╗ ██╗      ██████╗  ██████╗██╗  ██╗
+// ██╔══██╗████╗  ██║██╔═══██╗╚══██╔══╝██║  ██║██╔════╝██╔══██╗██╔══██╗██║     ██╔═══██╗██╔════╝██║ ██╔╝
+// ███████║██╔██╗ ██║██║   ██║   ██║   ███████║█████╗  ██████╔╝██████╔╝██║     ██║   ██║██║     █████╔╝
+// ██╔══██║██║╚██╗██║██║   ██║   ██║   ██╔══██║██╔══╝  ██╔══██╗██╔══██╗██║     ██║   ██║██║     ██╔═██╗
+// ██║  ██║██║ ╚████║╚██████╔╝   ██║   ██║  ██║███████╗██║  ██║██████╔╝███████╗╚██████╔╝╚██████╗██║  ██╗
+// ╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝    ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═════╝ ╚══════╝ ╚═════╝  ╚═════╝╚═╝  ╚═╝
+//
+
+/**
+ * @title ABPublisherRegistry
+ * @author Anotherblock Technical Team
+ * @notice Anotherblock Publisher Registry contract responsible for housekeeping publisher details
+ *
+ */
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+/* Openzeppelin Contract */
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ABPublisherRegistry is Ownable {
+    // /**
+    //  * @notice
+    //  *  Drop Structure format
+    //  *
+    //  * @param publisherId publisher identifier
+    //  * @param account publisher account address
+    //  * @param abRoyalty ABRoyalty address associated to the publisher
+    //  */
+    // struct Publisher {
+    //     address abRoyalty;
+    // }
+
+    /// @dev Error returned when caller is not authorized to perform operation
+    error FORBIDDEN();
+
+    /// @dev Event emitted when a new publisher is registered
+    event PublisherRegistered(address account, address abRoyalty);
+
+    //     _____ __        __
+    //    / ___// /_____ _/ /____  _____
+    //    \__ \/ __/ __ `/ __/ _ \/ ___/
+    //   ___/ / /_/ /_/ / /_/  __(__  )
+    //  /____/\__/\__,_/\__/\___/____/
+
+    /// @dev AnotherCloneFactory contract address
+    address public anotherCloneFactory;
+
+    /// @dev Mapping storing ABRoyalty contract address for a given publisher account
+    mapping(address publisher => address abRoyalty) public publishers;
+
+    //     ______                 __                  __
+    //    / ____/___  ____  _____/ /________  _______/ /_____  _____
+    //   / /   / __ \/ __ \/ ___/ __/ ___/ / / / ___/ __/ __ \/ ___/
+    //  / /___/ /_/ / / / (__  ) /_/ /  / /_/ / /__/ /_/ /_/ / /
+    //  \____/\____/_/ /_/____/\__/_/   \__,_/\___/\__/\____/_/
+
+    /**
+     * @notice
+     *  Contract Constructor
+     */
+    constructor() {}
+
+    //     ____        __         ___                                         __
+    //    / __ \____  / /_  __   /   |  ____  ____  _________ _   _____  ____/ /
+    //   / / / / __ \/ / / / /  / /| | / __ \/ __ \/ ___/ __ \ | / / _ \/ __  /
+    //  / /_/ / / / / / /_/ /  / ___ |/ /_/ / /_/ / /  / /_/ / |/ /  __/ /_/ /
+    //  \____/_/ /_/_/\__, /  /_/  |_/ .___/ .___/_/   \____/|___/\___/\__,_/
+    //               /____/         /_/   /_/
+
+    /**
+     * @notice
+     *  Register a new publisher
+     *  Only AnotherCloneFactory can perform this operation
+     *
+     * @param _publisher address of the publisher
+     * @param _abRoyalty address of ABRoyalty contract associated to this publisher
+     *
+     */
+    function registerPublisher(address _publisher, address _abRoyalty) external onlyFactory {
+        // Store the new publisher ABRoyalty contract address
+        publishers[_publisher] = _abRoyalty;
+
+        // Emit the PublisherRegistered event
+        emit PublisherRegistered(_publisher, _abRoyalty);
+    }
+
+    //     ____        __         ____
+    //    / __ \____  / /_  __   / __ \_      ______  ___  _____
+    //   / / / / __ \/ / / / /  / / / / | /| / / __ \/ _ \/ ___/
+    //  / /_/ / / / / / /_/ /  / /_/ /| |/ |/ / / / /  __/ /
+    //  \____/_/ /_/_/\__, /   \____/ |__/|__/_/ /_/\___/_/
+    //               /____/
+
+    /**
+     * @notice
+     *  Set AnotherCloneFactory contract address
+     *  Only the contract owner can perform this operation
+     *
+     * @param _anotherCloneFactory address of AnotherCloneFactory contract
+     *
+     */
+    function setAnotherCloneFactory(address _anotherCloneFactory) external onlyOwner {
+        anotherCloneFactory = _anotherCloneFactory;
+    }
+
+    //   _    ___                 ______                 __  _
+    //  | |  / (_)__ _      __   / ____/_  ______  _____/ /_(_)___  ____  _____
+    //  | | / / / _ \ | /| / /  / /_  / / / / __ \/ ___/ __/ / __ \/ __ \/ ___/
+    //  | |/ / /  __/ |/ |/ /  / __/ / /_/ / / / / /__/ /_/ / /_/ / / / (__  )
+    //  |___/_/\___/|__/|__/  /_/    \__,_/_/ /_/\___/\__/_/\____/_/ /_/____/
+
+    /**
+     * @notice
+     *  Return true if `_account` is a publisher, false otherwise
+     *
+     * @param _account address to be queried
+     *
+     * @return _isPublisher true if `_account` is a publisher, false otherwise
+     */
+    function isPublisher(address _account) external view returns (bool _isPublisher) {
+        _isPublisher = publishers[_account] != address(0);
+    }
+
+    //     ____      __                        __   ______                 __  _
+    //    /  _/___  / /____  _________  ____ _/ /  / ____/_  ______  _____/ /_(_)___  ____  _____
+    //    / // __ \/ __/ _ \/ ___/ __ \/ __ `/ /  / /_  / / / / __ \/ ___/ __/ / __ \/ __ \/ ___/
+    //  _/ // / / / /_/  __/ /  / / / / /_/ / /  / __/ / /_/ / / / / /__/ /_/ / /_/ / / / (__  )
+    // /___/_/ /_/\__/\___/_/  /_/ /_/\__,_/_/  /_/    \__,_/_/ /_/\___/\__/_/\____/_/ /_/____/
+
+    //      __  ___          ___ _____
+    //     /  |/  /___  ____/ (_) __(_)__  _____
+    //    / /|_/ / __ \/ __  / / /_/ / _ \/ ___/
+    //   / /  / / /_/ / /_/ / / __/ /  __/ /
+    //  /_/  /_/\____/\__,_/_/_/ /_/\___/_/
+
+    /**
+     * @notice
+     *  Ensure that the call is coming from AnotherCloneFactory contract
+     */
+    modifier onlyFactory() {
+        if (msg.sender != anotherCloneFactory) {
+            revert FORBIDDEN();
+        }
+        _;
+    }
+}
