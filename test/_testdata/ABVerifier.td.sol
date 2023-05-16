9979ffb51443a8ed14d114bba4bf07c0178fea1a
Pilou <76021631+0xPilou@users.noreply.github.com>
2023-05-16 14:58:29 +0200

    full coverage ABVerifier.sol

--- test/_testdata/ABVerifier.td.sol
+++ test/_testdata/ABVerifier.td.sol
@@ -7,4 +7,12 @@ contract ABVerifierTestData {
     bytes32 public constant AB_ADMIN_ROLE_HASH = keccak256("AB_ADMIN_ROLE");
 
     /* Test Data */
+
+    uint256 public constant PHASE_0 = 0;
+    uint256 public constant PHASE_1 = 1;
+    uint256 public constant PHASE_2 = 2;
+
+    uint256 public constant TOKEN_0 = 0;
+    uint256 public constant TOKEN_1 = 1;
+    uint256 public constant TOKEN_2 = 2;
 }
