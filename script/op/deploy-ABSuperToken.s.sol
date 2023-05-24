70ba9632b0262c23e2ad620a3881b2af697afdd2
Pilou <76021631+0xPilou@users.noreply.github.com>
2023-05-24 11:26:27 +0200

    fix warning

--- script/op/deploy-ABSuperToken.s.sol
+++ script/op/deploy-ABSuperToken.s.sol
@@ -7,7 +7,7 @@ import "test/_mocks/ABSuperToken.sol";
 contract DeployMockSuperToken is Script {
     error INCORRECT_NETWORK();
 
-    address SF_HOST_OPTIMISM_GOERLI = 0xE40983C2476032A0915600b9472B3141aA5B5Ba9;
+    address public constant SF_HOST_OPTIMISM_GOERLI = 0xE40983C2476032A0915600b9472B3141aA5B5Ba9;
 
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
