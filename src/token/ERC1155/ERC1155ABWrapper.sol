965cf89dff01af8a3b467ca8369eaf4c0ad2c2c9
Pilou <76021631+0xPilou@users.noreply.github.com>
2023-06-22 14:08:30 +0200

    remove unused import

--- src/token/ERC1155/ERC1155ABWrapper.sol
+++ src/token/ERC1155/ERC1155ABWrapper.sol
@@ -41,7 +41,6 @@ import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/acce
 import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
 /* Anotherblock Libraries */
-import {ABDataTypes} from "src/libraries/ABDataTypes.sol";
 import {ABErrors} from "src/libraries/ABErrors.sol";
 import {ABEvents} from "src/libraries/ABEvents.sol";
 
