93c3a274a5356deebb436448e46ca7843548db94
Pilou <76021631+0xPilou@users.noreply.github.com>
2023-06-22 14:56:20 +0200

    remove unused imports

--- script/base/create-publisher.s.sol
+++ script/base/create-publisher.s.sol
@@ -3,14 +3,7 @@ pragma solidity ^0.8.18;
 
 import "forge-std/Script.sol";
 
-import {ABDataRegistry} from "src/utils/ABDataRegistry.sol";
-import {ABHolderRegistry} from "src/utils/ABHolderRegistry.sol";
-import {ABVerifier} from "src/utils/ABVerifier.sol";
 import {AnotherCloneFactory} from "src/factory/AnotherCloneFactory.sol";
-import {ERC1155AB} from "src/token/ERC1155/ERC1155AB.sol";
-import {ERC721ABWrapper} from "src/token/ERC721/ERC721ABWrapper.sol";
-import {ERC721ABBase} from "src/token/ERC721/ERC721ABBase.sol";
-import {ERC1155ABWrapper} from "src/token/ERC1155/ERC1155ABWrapper.sol";
 
 contract CreatePublisher is Script {
     function run() external {
