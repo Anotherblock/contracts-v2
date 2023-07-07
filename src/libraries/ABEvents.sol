92367dc3244b57e21fd193d3de702e728cc388c3
Pilou <76021631+0xPilou@users.noreply.github.com>
2023-07-07 10:07:00 +0200

    remove uneccessary events

--- src/libraries/ABEvents.sol
+++ src/libraries/ABEvents.sol
@@ -45,12 +45,6 @@ library ABEvents {
     /// @dev Event emitted when a new drop is registered
     event DropRegistered(uint256 indexed dropId, uint256 indexed tokenId, address nft, address publisher);
 
-    /// @dev Event emitted upon wrapping of a token
-    event Wrapped(uint256 tokenId, uint256 quantity, address user);
-
-    /// @dev Event emitted upon unwrapping of a token
-    event Unwrapped(uint256 tokenId, uint256 quantity, address user);
-
     /// @dev Event emitted upon phase update
     event UpdatedPhase(uint256 numOfPhase);
 
