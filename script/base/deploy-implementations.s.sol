499bc45474e144536f6ee79e0e990368ffa06415
Pilou <76021631+0xPilou@users.noreply.github.com>
2023-08-07 17:05:09 +0200

    script for mainnet

---
+++ script/base/deploy-implementations.s.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "forge-std/Script.sol";
+
+import {ERC1155AB} from "src/token/ERC1155/ERC1155AB.sol";
+import {ERC721AB} from "src/token/ERC721/ERC721AB.sol";
+import {AnotherCloneFactory} from "src/factory/AnotherCloneFactory.sol";
+
+contract DeployImplementation is Script {
+    ERC721AB public erc721Impl;
+    ERC1155AB public erc1155Impl;
+    address public anotherCloneFactory = 0x137d7d27af9B4d7b467Ac008AFdcDb8C9Ac4ddd9;
+
+    function run() external {
+        // Account to deploy from
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        // Start broadcasting transactions
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Deploy Implementation Contracts
+        erc721Impl = new ERC721AB();
+        erc1155Impl = new ERC1155AB();
+
+        AnotherCloneFactory(anotherCloneFactory).setERC721Implementation(address(erc721Impl));
+        AnotherCloneFactory(anotherCloneFactory).setERC1155Implementation(address(erc1155Impl));
+
+        vm.stopBroadcast();
+    }
+}
