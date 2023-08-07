43b45f86aa029b3485c72f2ae394f95c278ae2a3
Pilou <76021631+0xPilou@users.noreply.github.com>
2023-08-07 17:04:12 +0200

    deploy script for ERC721 & ERC1155 implementation contracts

---
+++ script/base-goerli/deploy-implementations.s.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "forge-std/Script.sol";
+
+import {ERC1155AB} from "src/token/ERC1155/ERC1155AB.sol";
+import {ERC721AB} from "src/token/ERC721/ERC721AB.sol";
+import {AnotherCloneFactory} from "src/factory/AnotherCloneFactory.sol";
+
+contract DeployImplementation is Script {
+    ERC721AB public erc721Impl;
+    ERC1155AB public erc1155Impl;
+
+    function run(bool isDryRun) external {
+        // Account to deploy from
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        // Start broadcasting transactions
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Deploy Implementation Contracts
+        erc721Impl = new ERC721AB();
+        erc1155Impl = new ERC1155AB();
+
+        AnotherCloneFactory(0x4E393A0DD0331cE371d4d6fdd0B97E4b02450514).setERC721Implementation(address(erc721Impl));
+        AnotherCloneFactory(0x4E393A0DD0331cE371d4d6fdd0B97E4b02450514).setERC1155Implementation(address(erc1155Impl));
+
+        vm.stopBroadcast();
+    }
+}
