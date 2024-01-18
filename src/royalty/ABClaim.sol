945e720ff7f1bd8f4c9921d79d1b686fb098f0e8
Pilou <76021631+0xPilou@users.noreply.github.com>
2024-01-18 14:47:41 +0100

    improved ABClaim

--- src/royalty/ABClaim.sol
+++ src/royalty/ABClaim.sol
@@ -47,6 +47,12 @@ import {ABEvents} from "src/libraries/ABEvents.sol";
 /* anotherblock Interfaces */
 import {IERC721AB} from "src/token/ERC721/IERC721AB.sol";
 
+struct DropData {
+    address nft;
+    bool isL1;
+    uint256 supply;
+}
+
 contract ABClaim is Initializable, AccessControlUpgradeable {
     //     _____ __        __
     //    / ___// /_____ _/ /____  _____
@@ -54,14 +60,14 @@ contract ABClaim is Initializable, AccessControlUpgradeable {
     //   ___/ / /_/ /_/ / /_/  __(__  )
     //  /____/\__/\__,_/\__/\___/____/
 
-    uint256 public totalDeposited;
-    uint256 public tokenSupply;
-    address public nft;
-    address public publisher;
-
     IERC20 public USDC;
 
-    mapping(uint256 tokenId => uint256 amount) public claimedAmount;
+    mapping(uint256 dropId => uint256 totalDeposited) public totalDepositedPerDrop;
+    mapping(uint256 dropId => DropData dropData) public dropData;
+    mapping(uint256 dropId => mapping(uint256 tokenId => uint256 amount) claimedPerTokenId) public claimedAmount;
+    mapping(uint256 dropId => mapping(uint256 tokenId => address owner) ownerOf) public ownerOf;
+
+    bytes32 public constant RELAYER_ROLE = keccak256("RELAYER_ROLE");
 
     ///@dev ABClaim implementation version
     uint8 public constant IMPLEMENTATION_VERSION = 1;
@@ -85,16 +91,22 @@ contract ABClaim is Initializable, AccessControlUpgradeable {
      * @notice
      *  Contract Initializer
      *
-     * @param _publisher collection publisher address
      */
-    function initialize(address _publisher) external initializer {
+    function initialize(address _relayer) external initializer {
         // Initialize Access Control
         __AccessControl_init();
-        _grantRole(DEFAULT_ADMIN_ROLE, _publisher);
-        _revokeRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(RELAYER_ROLE, _relayer);
 
-        // Assign the publisher address
-        publisher = _publisher;
+        // Backfill existing drop data
+        /// TODO : get accurate data for all drops since genesis
+        dropData[1] = DropData(address(0), true, 100);
+        dropData[2] = DropData(address(0), true, 100);
+        dropData[3] = DropData(address(0), true, 100);
+        dropData[4] = DropData(address(0), true, 100);
+        dropData[5] = DropData(address(0), true, 100);
+        dropData[6] = DropData(address(0), true, 100);
+        dropData[7] = DropData(address(0), true, 100);
     }
 
     //     ______     __                        __   ______                 __  _
@@ -103,27 +115,70 @@ contract ABClaim is Initializable, AccessControlUpgradeable {
     //  / /____>  </ /_/  __/ /  / / / / /_/ / /  / __/ / /_/ / / / / /__/ /_/ / /_/ / / / (__  )
     // /_____/_/|_|\__/\___/_/  /_/ /_/\__,_/_/  /_/    \__,_/_/ /_/\___/\__/_/\____/_/ /_/____/
 
-    function claim(uint256 _tokenId) external {
-        if (IERC721AB(nft).ownerOf(_tokenId) != msg.sender) revert ABErrors.NOT_TOKEN_OWNER();
+    function claim(uint256[] calldata _dropIds, uint256[][] calldata _tokenIds) external {
+        uint256 dLength = _dropIds.length;
+        if (dLength != _tokenIds.length) revert ABErrors.INVALID_PARAMETER();
 
-        uint256 claimableAmount = getClaimableAmount(_tokenId);
+        uint256 totalClaimable;
 
-        claimedAmount[_tokenId] += claimableAmount;
-        USDC.transfer(msg.sender, claimableAmount);
+        for (uint256 i; i < dLength;) {
+            uint256 dropId = _dropIds[i];
+            DropData memory data = dropData[dropId];
+            uint256 royaltiesPerToken = totalDepositedPerDrop[dropId] / data.supply;
+
+            if (data.isL1) {
+                for (uint256 j; j < _tokenIds[i].length;) {
+                    if (ownerOf[dropId][_tokenIds[j][i]] != msg.sender) revert ABErrors.NOT_TOKEN_OWNER();
+                    uint256 _claimable = royaltiesPerToken - claimedAmount[dropId][_tokenIds[j][i]];
+                    totalClaimable += _claimable;
+                    claimedAmount[dropId][_tokenIds[j][i]] += _claimable;
+
+                    unchecked {
+                        ++j;
+                    }
+                }
+            } else {
+                for (uint256 j; j < _tokenIds[i].length;) {
+                    if (IERC721AB(data.nft).ownerOf(_tokenIds[j][i]) != msg.sender) revert ABErrors.NOT_TOKEN_OWNER();
+                    uint256 _claimable = royaltiesPerToken - claimedAmount[dropId][_tokenIds[j][i]];
+                    totalClaimable += _claimable;
+                    claimedAmount[dropId][_tokenIds[j][i]] += _claimable;
+
+                    unchecked {
+                        ++j;
+                    }
+                }
+            }
+            unchecked {
+                ++i;
+            }
+        }
+        USDC.transfer(msg.sender, totalClaimable);
     }
-    //    ____        __         ___       __          _
-    //   / __ \____  / /_  __   /   | ____/ /___ ___  (_)___
-    //  / / / / __ \/ / / / /  / /| |/ __  / __ `__ \/ / __ \
-    // / /_/ / / / / / /_/ /  / ___ / /_/ / / / / / / / / / /
-    // \____/_/ /_/_/\__, /  /_/  |_\__,_/_/ /_/ /_/_/_/ /_/
-    //              /____/
 
-    //     ____        __         ____             _      __
-    //    / __ \____  / /_  __   / __ \___  ____ _(_)____/ /________  __
-    //   / / / / __ \/ / / / /  / /_/ / _ \/ __ `/ / ___/ __/ ___/ / / /
-    //  / /_/ / / / / / /_/ /  / _, _/  __/ /_/ / (__  ) /_/ /  / /_/ /
-    //  \____/_/ /_/_/\__, /  /_/ |_|\___/\__, /_/____/\__/_/   \__, /
-    //               /____/              /____/                /____/
+    /// TODO add claim function for single drop claiming (save gas for users only holding one drop)
+    function claim(uint256 _dropId, uint256[] calldata _tokenId) external {}
+
+    //      ____        __         ___       __          _
+    //     / __ \____  / /_  __   /   | ____/ /___ ___  (_)___
+    //    / / / / __ \/ / / / /  / /| |/ __  / __ `__ \/ / __ \
+    //   / /_/ / / / / / /_/ /  / ___ / /_/ / / / / / / / / / /
+    //   \____/_/ /_/_/\__, /  /_/  |_\__,_/_/ /_/ /_/_/_/ /_/
+    //                /____/
+
+    function depositRoyalty(uint256 _dropId, uint256 _amount) external {}
+    function depositRoyalty(uint256[] calldata _dropId, uint256[] calldata _amount) external {}
+
+    //     ____        __         ____       __
+    //    / __ \____  / /_  __   / __ \___  / /___ ___  _____  _____
+    //   / / / / __ \/ / / / /  / /_/ / _ \/ / __ `/ / / / _ \/ ___/
+    //  / /_/ / / / / / /_/ /  / _, _/  __/ / /_/ / /_/ /  __/ /
+    //  \____/_/ /_/_/\__, /  /_/ |_|\___/_/\__,_/\__, /\___/_/
+    //               /____/                      /____/
+
+    function updateL1Holdings(uint256 _dropId, uint256 _tokenId, address _newOwner) external onlyRole(RELAYER_ROLE) {
+        ownerOf[_dropId][_tokenId] = _newOwner;
+    }
 
     //   _    ___                 ______                 __  _
     //  | |  / (_)__ _      __   / ____/_  ______  _____/ /_(_)___  ____  _____
@@ -131,14 +186,17 @@ contract ABClaim is Initializable, AccessControlUpgradeable {
     //  | |/ / /  __/ |/ |/ /  / __/ / /_/ / / / / /__/ /_/ / /_/ / / / (__  )
     //  |___/_/\___/|__/|__/  /_/    \__,_/_/ /_/\___/\__/_/\____/_/ /_/____/
 
-    function getClaimableAmount(uint256 _tokenId) external view returns (uint256 _claimable) {
-        _claimable = _getClaimableAmount(_tokenId);
-    }
+    function getClaimableAmount(uint256 _dropId, uint256[] calldata _tokenIds)
+        external
+        view
+        returns (uint256 _totalClaimable)
+    {
+        DropData memory data = dropData[_dropId];
+        uint256 royaltiesPerToken = totalDepositedPerDrop[_dropId] / data.supply;
 
-    function getClaimableAmount(uint256[] calldata _tokenIds) external view returns (uint256 _claimable) {
-        uint256 length = _tokenIds.length;
-        for (uint256 i = 0; i < length;) {
-            _claimable += _getClaimableAmount(_tokenIds[i]);
+        for (uint256 i; i < _tokenIds.length;) {
+            uint256 _claimable = royaltiesPerToken - claimedAmount[_dropId][_tokenIds[i]];
+            _totalClaimable += _claimable;
 
             unchecked {
                 ++i;
@@ -146,27 +204,37 @@ contract ABClaim is Initializable, AccessControlUpgradeable {
         }
     }
 
-    //     ____      __                        __   ______                 __  _
-    //    /  _/___  / /____  _________  ____ _/ /  / ____/_  ______  _____/ /_(_)___  ____  _____
-    //    / // __ \/ __/ _ \/ ___/ __ \/ __ `/ /  / /_  / / / / __ \/ ___/ __/ / __ \/ __ \/ ___/
-    //  _/ // / / / /_/  __/ /  / / / / /_/ / /  / __/ / /_/ / / / / /__/ /_/ / /_/ / / / (__  )
-    // /___/_/ /_/\__/\___/_/  /_/ /_/\__,_/_/  /_/    \__,_/_/ /_/\___/\__/_/\____/_/ /_/____/
+    function getClaimableAmount(uint256[] calldata _dropIds, uint256[][] calldata _tokenIds)
+        external
+        view
+        returns (uint256 _totalClaimable)
+    {
+        uint256 dLength = _dropIds.length;
+        if (dLength != _tokenIds.length) revert ABErrors.INVALID_PARAMETER();
 
-    function _getClaimableAmount(uint256 _tokenId) internal view returns (uint256 _claimable) {
-        uint256 royaltiesPerToken = totalDeposited / tokenSupply;
-        _claimable = royaltiesPerToken - claimedAmount[_tokenId];
-    }
+        for (uint256 i; i < dLength;) {
+            uint256 dropId = _dropIds[i];
+            DropData memory data = dropData[dropId];
+            uint256 royaltiesPerToken = totalDepositedPerDrop[dropId] / data.supply;
 
-    function _getClaimableAmount(uint256[] calldata _tokenIds) internal view returns (uint256 _claimable) {
-        uint256 royaltiesPerToken = totalDeposited / tokenSupply;
-        uint256 length = _tokenIds.length;
-        
-        for (uint256 i = 0; i < length;) {
-            _claimable += royaltiesPerToken - claimedAmount[_tokenIds[i]];
+            for (uint256 j; j < _tokenIds[i].length;) {
+                uint256 _claimable = royaltiesPerToken - claimedAmount[dropId][_tokenIds[j][i]];
+                _totalClaimable += _claimable;
 
+                unchecked {
+                    ++j;
+                }
+            }
             unchecked {
                 ++i;
             }
         }
     }
 }
+
+//    ____        __         ___       __          _
+//   / __ \____  / /_  __   /   | ____/ /___ ___  (_)___
+//  / / / / __ \/ / / / /  / /| |/ __  / __ `__ \/ / __ \
+// / /_/ / / / / / /_/ /  / ___ / /_/ / / / / / / / / / /
+// \____/_/ /_/_/\__, /  /_/  |_\__,_/_/ /_/ /_/_/_/ /_/
+//              /____/
